8122915701f2aa24e79cea2ad2563a42b5fcaa60
Gary Pang <gary.pang@gmail.com>
2016-11-20 02:18:37 -0500

    Add audio
    Add sound effects to game.

--- game.js
+++ game.js
@@ -15,6 +15,11 @@ BasicGame.Game.prototype = {
     this.load.spritesheet('boss', 'assets/boss.png', 93, 75);
     this.load.spritesheet('explosion', 'assets/explosion.png', 32, 32);
     this.load.spritesheet('player', 'assets/player.png', 64, 64);
+    this.load.audio('explosion', ['assets/explosion.ogg', 'assets/explosion.wav']);
+    this.load.audio('playerExplosion', ['assets/player-explosion.ogg', 'assets/player-explosion.wav']);
+    this.load.audio('enemyFire', ['assets/enemy-fire.ogg', 'assets/enemy-fire.wav']);
+    this.load.audio('playerFire', ['assets/player-fire.ogg', 'assets/player-fire.wav']);
+    this.load.audio('powerUp', ['assets/powerup.ogg', 'assets/powerup.wav']);
   },
 
 
@@ -26,6 +31,7 @@ BasicGame.Game.prototype = {
     this.setupExplosions();
     this.setupPlayerIcons();
     this.setupText();
+    this.setupAudio();
 
     // Allow keyboard control
     this.cursors = this.input.keyboard.createCursorKeys();
@@ -189,6 +195,7 @@ BasicGame.Game.prototype = {
     }
 
     this.nextShotAt = this.time.now + this.shotDelay;
+    this.playerFireSFX.play();
 
     var bullet;
     if (this.weaponLevel === 0) {
@@ -231,6 +238,7 @@ BasicGame.Game.prototype = {
           bullet, this.player, BasicGame.ENEMY_BULLET_VELOCITY
           );
         enemy.nextShotAt = this.time.now + BasicGame.SHOOTER_SHOT_DELAY;
+        this.enemyFireSFX.play();
       }
     }, this);
 
@@ -239,6 +247,7 @@ BasicGame.Game.prototype = {
       this.enemyBulletPool.countDead() >= 10) {
 
       this.boss.nextShotAt = this.time.now + BasicGame.BOSS_SHOT_DELAY;
+      this.enemyFireSFX.play();
 
     for (var i = 0; i < 5; i++) {
       // Process 2 bullets at a time
@@ -273,6 +282,9 @@ BasicGame.Game.prototype = {
     if (this.ghostUntil && this.ghostUntil > this.time.now) {
       return;
     }
+
+    this.playerExplosionSFX.play();
+
     // Crashing into enemy only deals 5 damages
     this.damageEnemy(enemy, BasicGame.CRASH_DAMAGE);
     var life = this.lives.getFirstAlive();
@@ -493,6 +505,15 @@ BasicGame.Game.prototype = {
     this.scoreText.anchor.setTo(0.5, 0.5);
   },
 
+  setupAudio: function() {
+    this.sound.volume = 0.3;
+    this.explosionSFX = this.add.audio('explosion');
+    this.playerExplosionSFX = this.add.audio('playerExplosion');
+    this.enemyFireSFX = this.add.audio('enemyFire');
+    this.playerFireSFX = this.add.audio('playerFire');
+    this.powerUpSFX = this.add.audio('powerUp');    
+  },
+
 
   // When bullet hits enemy
   enemyHit: function(bullet, enemy) {
@@ -521,6 +542,7 @@ BasicGame.Game.prototype = {
       enemy.play('hit');
     } else {
       this.explode(enemy);
+      this.explosionSFX.play();
       this.spawnPowerUp(enemy);
       this.addToScore(enemy.reward);
 
@@ -570,6 +592,7 @@ BasicGame.Game.prototype = {
   playerPowerUp: function(player, powerUp) {
     this.addToScore(powerUp.reward);
     powerUp.kill();
+    this.powerUpSFX.play();
     if (this.weaponLevel < 5) {
       this.weaponLevel++;
     }
